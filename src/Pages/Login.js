76cca2202ff5693949ec3dc76e1c13013e551c41
Your Name <you@example.com>
2022-02-15 17:37:07 +0500

    form validation

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,33 +1,20 @@
 import React from 'react';
 import { useFormik } from 'formik';
-
-const validate = values => {
-  const errors = {};
-  
-  if (!values.email) {
-    errors.email = 'Required';
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    errors.email = 'Invalid email address';
-  }
-
-  if (!values.password) {
-    errors.password = 'Required';
-  } else if (values.password.length < 6) {
-    errors.password = 'password Must be 6 characters or greater';
-  }
-
-  return errors;
-};
+import * as Yup from 'yup';
 
 const Login =() => {
 
-  const { handleSubmit, handleBlur, handleChange, handleReset, errors, values} = useFormik({
+  const { handleSubmit, handleBlur, handleChange, errors, values,touched} = useFormik({
      
       initialValues:{
         email: '',
         password:''
       },
-      validate,
+      validationSchema: Yup.object({
+        email: Yup.string().email('Please enter a valid email address').required('Please enter your email to proceed'),
+        password: Yup.string().required('Please enter the password').matches('^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8}$', 'password must have 2 uppercase, 2 digits 3 lowecase and the length of 8 characters')
+      }),
+
       onSubmit: (values) => {
         console.log(values)
       }
@@ -41,21 +28,23 @@ const Login =() => {
         name="email"
         type="text"
         onChange={handleChange}
+        onBlur={handleBlur}
         value={values.email}
         className= "form-control"
       />
-      {errors.email ? <div className='text-danger'>{errors.email}</div> : null}
+      {touched.email && errors.email ? <div className='text-danger'>{errors.email}</div> : null}
 
-      <label htmlFor="password">Last Name</label>
+      <label htmlFor="password">Password</label>
       <input
         id="password"
         name="password"
-        type="text"
+        type="password"
         onChange={handleChange}
+        onBlur={handleBlur}
         value={values.password}
         className= "form-control"
       />
-      {errors.password ? <div className='text-danger'>{errors.password}</div> : null}
+      {touched.password && errors.password ? <div className='text-danger'>{errors.password}</div> : null}
 
       <button type="submit" className='btn btn-primary w-100 mt-2'>Submit</button>
     </form>
